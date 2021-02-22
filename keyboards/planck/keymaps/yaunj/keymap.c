7c7b9a0954e05c118d1dee21b6ad588809642e8e
Rune Hammersland <rune@hammersland.net>
2021-02-22 21:24:24 +0100

    Improve planck keymap.
    While experimenting with a numpad layer, I forgot to readd brackets
    etc. This commit also removes arrows from default layer and adds
    right alt (which I use as ISO level 3 shift).

--- keyboards/planck/keymaps/yaunj/keymap.c
+++ keyboards/planck/keymaps/yaunj/keymap.c
@@ -24,6 +24,7 @@ enum planck_layers {
   _DVORAK,
   _LOWER,
   _RAISE,
+  _NUMPAD,
   _NAV,
   _PLOVER,
   _ADJUST
@@ -63,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
-    XXXXXXX, KC_APP,  KC_LGUI, KC_LALT, LOWER,  SPACEFN, SPACEFN,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    XXXXXXX, KC_APP,  KC_LGUI, KC_LALT, LOWER,  SPACEFN, SPACEFN,  RAISE,   KC_RALT, KC_RGUI, KC_APP,  MO(_NUMPAD)
 ),
 
 /* Colemak
@@ -81,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
     CTL_ESC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
-    XXXXXXX, KC_APP,  KC_LGUI, KC_LALT, LOWER,  SPACEFN, SPACEFN,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 /* Dvorak
@@ -99,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC,
     CTL_ESC, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH,
     KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_SFTENT,
-    XXXXXXX, KC_APP,  KC_LGUI, KC_LALT, LOWER,  SPACEFN, SPACEFN,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 /* Lower
@@ -139,26 +140,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
-/*
 [_RAISE] = LAYOUT_planck_grid(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
-*/
 
-[_RAISE] = LAYOUT_planck_grid(
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F12,  XXXXXXX, XXXXXXX, KC_SLSH,  KC_7,    KC_8,    KC_9,   KC_BSPC,
-    _______, KC_F4,   KC_F5,   KC_F6,   KC_F11,  XXXXXXX, XXXXXXX, KC_MINS,  KC_4,    KC_5,    KC_6,   KC_PLUS,
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F10,  XXXXXXX, XXXXXXX, KC_COMM,  KC_1,    KC_2,    KC_3,   _______,
-    _______, _______, _______, _______, _______, _______, _______, _______,  KC_0,  KC_DOT, _______,   _______
+[_NUMPAD] = LAYOUT_planck_grid(
+    _______, _______, _______, _______, _______, _______, _______, KC_SLSH,  KC_7,    KC_8,    KC_9,   KC_BSPC,
+    _______, _______, _______, _______, _______, _______, _______, KC_MINS,  KC_4,    KC_5,    KC_6,   KC_PLUS,
+    _______, _______, _______, _______, _______, _______, _______, KC_COMM,  KC_1,    KC_2,    KC_3,   _______,
+    _______, _______, _______, _______, _______, _______, _______, _______,  KC_0,  KC_DOT, XXXXXXX,   _______
 ),
 
 [_NAV] = LAYOUT_planck_grid(
-    _______, KC_PGUP, KC_UP,   KC_PGDN, XXXXXXX, XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, _______,
+    _______, KC_PGUP, KC_UP,   KC_PGDN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, SFT_INS, XXXXXXX, XXXXXXX, _______,
     _______, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, _______,
-    _______, XXXXXXX, XXXXXXX, CTL_INS, SFT_INS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+    _______, XXXXXXX, XXXXXXX, CTL_INS, SFT_INS, XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
